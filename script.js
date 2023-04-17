73b1d41617dcebaab3dcd4f13544016af7f4e872
Will Kelly <willkelly92@gmail.com>
2023-04-17 18:11:32 +0100

    Add HTML and JS file
    -Adds standard HTML file
    -Add JS File in pseudocode

---
+++ script.js
@@ -0,0 +1,38 @@
+//getComputerchoice //randomly return rock paper or scissors
+
+
+function getComputerchoice
+//prompt() getplayerchoice //returns choice of rock paper or scissosrs
+//IF computer choses rock and player chooses scissors
+//Print - you lose 
+//If computer choses rock and player chooses paper 
+//Print you win 
+//If computer choses rock and player chooses rock /
+//Print it's a draw
+
+//Repeat for all combinations
+
+
+//function playRound(playerSelection, computerSelection) {
+    // your code here!
+//  }
+   
+  //const playerSelection = "rock";
+  //const computerSelection = getComputerChoice();
+  //console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
